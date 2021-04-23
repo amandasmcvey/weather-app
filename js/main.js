c7b6ddb4bc94909eaa2e55bc637d2546b9f7fc80
amandasmcvey <amandamcvey11@gmail.com>
2021-04-23 17:53:31 -0500

    expanded on get weather ajax, added current location functionality

--- js/main.js
+++ js/main.js
@@ -2,43 +2,71 @@ const app = new Vue({
     el: "#app",
     data: {
 
+    },
+    methods: {
+        getMetaWeatherAjax(method, data) {
+            
+            var apiUrl = "";
+
+            switch (method) {
+                case locSearch:
+
+                    if (data.coords) {
+                        apiUrl = "search/?lattlong=";
+                        apiUrl += data.coords.lat + ',';
+                        apiUrl += data.coords.long;
+                    } else if (data.query) {
+                        apiUrl = "search/?query=";
+                        apiUrl += data.query;
+                    }
+                    
+                    break;
+                case todayForecast:
+
+                    var todayDate = new Date();
+                    var dd = String(today.getDate());
+                    var mm = String(today.getMonth() + 1);
+                    var yyyy = today.getFullYear();
+
+                    todayDate = yyyy + '/' + mm + '/' + dd;
+                    apiUrl = data.woeid + "/" + todayDate;
+                    break;
+            
+                default:
+
+                    break;
+            }
+
+            $.ajax({
+                url: "https://www.metaweather.com/api/location/" + apiUrl,
+                method: 'GET',
+                success: function (data) {
+                    if (method === "locSearch") {
+                        getMetaWeatherAjax("todayForecast", data);
+                    }
+                    
+                },
+                error: function(error){
+                    console.error(JSON.stringify(error));
+                }
+            });
+        },
+
+        getCurrLoc() {
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition((pos) => {
+                    var lat = pos.coords.latitude;
+                    var long = pos.coords.longitude;
+                    getMetaWeatherAjax("locSearch", { coords: { lat, long } });
+                });
+            } else {
+                // "Geolocation is not supported by this browser.";
+            }
+        }
     }
 });
+function
 
-function getMetaWeatherAjax(method, data) {
-    
-    var apiUrl = "";
 
-    switch (method) {
-        case locSearch:
-            apiUrl = "search/?query=";
-            break;
-        case todayForecast:
-            var todayDate = new Date();
-            var dd = String(today.getDate());
-            var mm = String(today.getMonth() + 1);
-            var yyyy = today.getFullYear();
-
-            todayDate = yyyy + '/' + mm + '/' + dd;
-            apiUrl = data.woeid + "/" + todayDate;
-            break;
-    
-        default:
-
-            break;
-    }
-
-    $.ajax({
-        url: "https://www.metaweather.com/api/location/" + apiUrl,
-        method: 'GET',
-        success: function (data) {
-            if (method === "locSearch") {
-                getMetaWeatherAjax("todayForecast", data);
-            }
-            
-        },
-        error: function(error){
-            console.error(JSON.stringify(error));
-        }
-    });
-}
+var name = "string";
+name ;
